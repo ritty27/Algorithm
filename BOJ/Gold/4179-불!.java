7f2ece466dd61604a6fb75c61d8f3819c3434677
JisooPark27 <jspark9812@gmail.com>
2021-06-22 18:03:42 +0900

    Refactoring 21.06.22

--- BOJ/Gold/4179-불!.java
+++ BOJ/Gold/4179-불!.java
@@ -59,7 +59,10 @@ public class Main {
             for (int i = 0; i < 4; i++) {
                 int curX = p.x + dx[i];
                 int curY = p.y + dy[i];
-                if (curX < 0 || curX >= n || curY < 0 || curY >= m) continue;
+                if (curX < 0 || curX >= n || curY < 0 || curY >= m) {
+                    System.out.println(jDistance[p.x][p.y] + 1);
+                    return;
+                }
                 if (jDistance[curX][curY] > 0 || board[curX][curY] != 0) continue;
                 if (jDistance[p.x][p.y] + 1 >= fDistance[curX][curY] && fVisited[curX][curY] == true) continue;
                 jDistance[curX][curY] = jDistance[p.x][p.y] + 1;
@@ -67,32 +70,7 @@ public class Main {
                 qu.add(new Point(curX, curY));
             }
         }
+        System.out.println("IMPOSSIBLE");
 
-        for (int i = 0; i < n; i++) {
-            if (jDistance[i][0] != 0 && jDistance[i][0] < min)
-                min = jDistance[i][0];
-            if (jDistance[i][m - 1] != 0 && jDistance[i][m - 1] < min)
-                min = jDistance[i][m - 1];
-        }
-
-        for (int i = 0; i < m; i++) {
-            if (jDistance[0][i] != 0 && jDistance[0][i] < min)
-                min = jDistance[0][i];
-            if (jDistance[n - 1][i] != 0 && jDistance[n - 1][i] < min)
-                min = jDistance[n - 1][i];
-
-        }
-
-        if (min == MAX_NUM) {
-            if (start.x == 0 || start.x == n - 1 || start.y == 0 || start.y == m - 1) {
-                System.out.println(1);
-                return;
-            } else {
-                System.out.println("IMPOSSIBLE");
-                return;
-            }
-        }
-
-        System.out.println(min + 1);
     }
 }
